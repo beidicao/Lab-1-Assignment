a8ee2d115b5e85405e3e3d77b0ed0c3651d471bb
beidicao <28caob@albanyacademy.org>
2026-09-15 12:56:56 +0000

    not done

--- Lab1.java
+++ Lab1.java
@@ -1,12 +1,17 @@
-/* Enter your header here
- * 
- * 
+/* Beidi Cao
+ * lab 1- Beginning program to practice using print and println statements 
+ * 9/15/2026
  */
 
 
 public class Lab1 {
     public static void main(String[] args) {
        //Enter your code here:
+       system.output.println("My name is beidi. I am 15 years old")
+       system.output.println("I love icecream!")
+       system.output.println("My favorite color is sky blue.")
+       system.output.println("I hate cold milk and egg.")
+       system.output.println("There are four people in my family.")
        
    }
 }
